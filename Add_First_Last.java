3ad736c459f6f93395e751c059a924d40775a7e3
Aarish <aarish3344@gmail.com>
2025-02-13 21:54:42 +0530

    initial commit - Update

--- Add_First_Last.java
+++ Add_First_Last.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Add_First_Last {
     public static void main(String[] args) {
         Scanner Add = new Scanner(System.in);
-        System.out.print("Enter Any four Digit Number: ");
+        System.out.print("Enter Any Four Digit Number: ");
         int Number = Add.nextInt();
         int Result = Number / 1000;
         int Result_1 = Number % 10;
