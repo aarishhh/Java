7d1e82e57f39198182119e95df4bd4c31b4e50ec
Aarish <aarish3344@gmail.com>
2025-02-18 01:56:35 +0530

    New

--- Q45_Maximum_Number_Of_Array.java
+++ Q45_Maximum_Number_Of_Array.java
@@ -1,27 +1,32 @@
+
 //WAP To Find Maximum Value In Given 1D Array. Take Number Of Elements From User. 
 import java.util.Scanner;
+
 public class Q45_Maximum_Number_Of_Array {
     Scanner Maximum = new Scanner(System.in);
-    int Num; 
+    int Num;
     int Number[];
+
     void Input() {
         System.out.print("Enter Number Of Elements: ");
         Num = Maximum.nextInt();
         Number = new int[Num];
         System.out.print("Enter The Elements: ");
-        for(int i = 0; i < Num; i++) {
+        for (int i = 0; i < Num; i++) {
             Number[i] = Maximum.nextInt();
         }
     }
+
     void Maximum_Number() {
         int Max = Number[0];
-        for(int i = 0; i < Num; i++) {
-            if(Number[i] > Max) {
+        for (int i = 0; i < Num; i++) {
+            if (Number[i] > Max) {
                 Max = Number[i];
             }
         }
-        System.out.println("The Maximum Value Is "+ Max);
+        System.out.println("The Maximum Value Is " + Max);
     }
+
     public static void main(String[] args) {
         Q45_Maximum_Number_Of_Array obj = new Q45_Maximum_Number_Of_Array();
         obj.Input();
