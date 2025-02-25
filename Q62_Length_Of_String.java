3c1b30e672c4347ff2e6dc7b1a7ad6a06526c704
Aarish <aarish3344@gmail.com>
2025-02-26 00:46:45 +0530

    New

---
+++ Q62_Length_Of_String.java
@@ -0,0 +1,19 @@
+//WAP To Find The Length Of Given String.
+import java.util.Scanner;
+
+public class Q62_Length_Of_String {
+    Scanner String_Length = new Scanner(System.in);
+    String Str = new String();
+    void Check_Length() {
+        System.out.print("Enter A String: ");
+        Str = String_Length.next();
+        int Length = Str.length();
+        System.out.println("The Length Of The Given String Is: " + Length);
+    }
+    public static void main(String[] args) {
+        while(true) {
+            Q62_Length_Of_String Obj = new Q62_Length_Of_String();
+            Obj.Check_Length();
+        }
+    }
+}
