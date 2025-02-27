f7780bc154a7d67e76f452fdec823f99b1f2e818
Aarish <aarish3344@gmail.com>
2025-02-27 21:27:52 +0530

    New

--- Q58_Reverse_String.java
+++ Q58_Reverse_String.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 public class Q58_Reverse_String {
     Scanner Reverse = new Scanner(System.in); 
     String Strr = new String();
+    String ReversedStr = "";
     void Reversing() {
         System.out.print("Enter A String: ");
         Strr = Reverse.next();
-        StringBuilder Str = new StringBuilder(Strr);
+        /*StringBuilder Str = new StringBuilder(Strr);
         Str.reverse();
-        System.out.println("Reversed String: " + Str);
+        System.out.println("Reversed String: " + Str);*/
+        for(int i = Strr.length() - 1; i >= 0; i--) {
+            ReversedStr += Strr.charAt(i);
+        }
+        System.out.println("Reversed String: " + ReversedStr);
     }
     public static void main(String[] args) {
         Q58_Reverse_String Obj = new Q58_Reverse_String();
