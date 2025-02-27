84d220d6e9bd308ddcbc18d5b79979f355291fe0
Aarish <aarish3344@gmail.com>
2025-02-27 21:29:21 +0530

    New

--- Q58_Reverse_String.java
+++ Q58_Reverse_String.java
@@ -1,21 +1,26 @@
+
 //WAP To Reverse The Given String.
 import java.util.Scanner;
 
 public class Q58_Reverse_String {
-    Scanner Reverse = new Scanner(System.in); 
+    Scanner Reverse = new Scanner(System.in);
     String Strr = new String();
     String ReversedStr = "";
+
     void Reversing() {
         System.out.print("Enter A String: ");
         Strr = Reverse.next();
-        /*StringBuilder Str = new StringBuilder(Strr);
-        Str.reverse();
-        System.out.println("Reversed String: " + Str);*/
-        for(int i = Strr.length() - 1; i >= 0; i--) {
+        /*
+         * StringBuilder Str = new StringBuilder(Strr);
+         * Str.reverse();
+         * System.out.println("Reversed String: " + Str);
+         */
+        for (int i = Strr.length() - 1; i >= 0; i--) {
             ReversedStr += Strr.charAt(i);
         }
         System.out.println("Reversed String: " + ReversedStr);
     }
+
     public static void main(String[] args) {
         Q58_Reverse_String Obj = new Q58_Reverse_String();
         Obj.Reversing();
