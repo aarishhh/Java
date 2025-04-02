b04f19e5947fb2a63e4d0af10a99f8e4214497cc
Aarish <aarish3344@gmail.com>
2025-04-02 16:12:30 +0530

    New

--- Q00_OverLoading.java
+++ Q00_OverLoading.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
 public class Q00_OverLoading {
-    int Num1, Num2;
-    int Addition() {
-        Scanner Add = new Scanner(System.in);
+    float Num1, Num2;
+    void Input() {
+        Scanner Calculation = new Scanner(System.in);
         System.out.println("Enter Two Numbers");
         System.out.print("Enter 1st Number: ");
-        int Num1 = Add.nextInt();
+        Num1 = Calculation.nextFloat();
         System.out.print("Enter 2nd Number: ");
-        int Num2 = Add.nextInt();
-        int Result = Num1 + Num2;
-        Add.close();
+        Num2 = Calculation.nextFloat(); 
+        Calculation.close();
+    }
+    float Addition(float num1, float num2) {
+        float Result = Num1 + Num2;
+        return Result;
+    }
+
+    float Multiplication(float num1, float num2) {
+        float Result = Num1 * Num2;
         return Result;
     }
 
     public static void main(String[] args) {
         Q00_OverLoading Obj = new Q00_OverLoading();
-        System.out.print("Addition = " + Obj.Addition());
+        Obj.Input();
+        System.out.printf("Addition = %.2f\n", Obj.Addition(0, 0));
+        System.out.printf("Multiplication = %.2f", Obj.Multiplication(0, 0));
     }
 }
